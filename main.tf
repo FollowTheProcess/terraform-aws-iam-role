90ef266d474a7fc4f5799533560f35bc7b17124a
Tom Fleet <me@followtheprocess.codes>
2025-07-05 09:31:03 +0100

    Fix refs to policy

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_iam_policy" "this" {
 
 resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this.name
-  policy_arn = aws_iam_policy.this.arn
+  policy_arn = aws_iam_policy.this[0].arn
 }
 
 resource "aws_iam_role_policy_attachment" "managed_policy" {
